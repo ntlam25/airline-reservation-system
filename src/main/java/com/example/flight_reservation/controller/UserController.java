d133d83d32e89847f9c40ab6d8fbdb5df8a213e4
ntlam25 <lam2k3d@gmail.com>
2024-11-26 23:31:47 +0700

    add User find all

--- src/main/java/com/example/flight_reservation/controller/UserController.java
+++ src/main/java/com/example/flight_reservation/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -27,6 +29,10 @@ public class UserController {
     @GetMapping("/search")
     public UserResponse findByEmail(@RequestParam String email){
         return service.findUserByEmail(email);
+    }
+    @GetMapping("/all")
+    public List<UserResponse> getUsers(){
+        return service.findAll();
     }
     @PutMapping("/{id}")
     public UserResponse updateById(@PathVariable Long id, @RequestBody UserRequest request){
